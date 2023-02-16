d74d50ea48bb575fce142e160ec5a309562ef045
mdpais <mdpais@gmail.com>
2023-02-16 19:27:55 -0400

    Fixed /orders pull route and /orders push route

--- controllers/api/orderRoutes.js
+++ controllers/api/orderRoutes.js
@@ -6,7 +6,7 @@ router.get('/', async (req, res) => {
   try {
     const orderData = await Order.findAll({
       // JOIN with ordered items
-      include: [{ model: User }, { model: Menu, through: OrderedItems }]
+      include: [{ model: User }, { model: OrderedItems, include: [{ model: Menu }] }]
     });
     res.status(200).json(orderData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
   try {
     const orderData = await Order.findByPk(req.params.id, {
       // JOIN with ordered items
-      include: [{ model: User }, { model: Menu, through: OrderedItems }]
+      include: [{ model: User }, { model: OrderedItems, include: [{ model: Menu }] }]
     });
 
     if (!orderData) {
@@ -35,29 +35,26 @@ router.get('/:id', async (req, res) => {
 
 // CREATE an order
 router.post('/', async (req, res) => {
-  // try {
-  //   const orderData = await Order.create(req.body);
-  //   res.status(200).json(orderData);
-  // } catch (err) {
-  //   res.status(400).json(err);
-  // }
+  console.log(req.body);
   Order.create(req.body)
   .then((order) => {
-    // create pairings of menu items included in order through bulk create in the OrderedItems model
-    if (req.body.orderedItemIds.length) {
-      const orderedItemIdArr = req.body.orderedItemIds.map((menu_id) => {
+    // create pairings of menu items and quantity included in order through bulk create in the OrderedItems model
+    if (req.body.menuIds.length) {
+      console.log(req.body.qty);
+      const menuIdArr = req.body.menuIds.map((menu_id, index) => {
         return {
           order_id: order.id,
           menu_id,
-          quantity,
+          quantity: req.body.qty[index],
         };
       });
-      return OrderedItems.bulkCreate(orderedItemIdArr);
+      console.log(menuIdArr);
+      return OrderedItems.bulkCreate(menuIdArr);
     }
     // if order is empty, create empty order
     res.status(200).json(order);
   })
-  .then((orderedItemIds) => res.status(200).json(orderedItemIds))
+  .then((menuIds) => res.status(200).json(menuIds))
   .catch((err) => {
     console.log(err);
     res.status(400).json(err);
@@ -65,24 +62,6 @@ router.post('/', async (req, res) => {
 });
 
 // UPDATE an order
-// router.put('/:id', async (req, res) => {
-//   try {
-//     const orderData = await Order.update(req.body, {
-//       where: {
-//         id: req.params.id
-//       }
-//     });
-
-//     if (!orderData) {
-//       res.status(404).json({ message: 'No order found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json({ message: 'Updated!' });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 router.put('/:id', async(req, res) => {
   // update product data
   const orders = await Order.update(req.body, {
