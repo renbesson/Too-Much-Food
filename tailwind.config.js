5226a15950dc5237726ce876a92ae785d2cf55e5
Ren Besson <ren.besson@outlook.com>
2023-02-18 16:31:26 -0400

    Tailwindcss+DaisyUI installed

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "rgb(190 99 45)",
+
+          secondary: "rgb(227 168 115)",
+
+          accent: "#f4a4d8",
+
+          neutral: "#23212C",
+
+          "base-100": "rgb(125 53 25)",
+
+          info: "#89BFEB",
+
+          success: "#137144",
+
+          warning: "#F4D371",
+
+          error: "#FA0A32",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
