588a28dd96239f941bb4a0ab7c266a555f3a6ab1
mdpais <mdpais@gmail.com>
2023-02-18 08:42:42 -0400

    Added home menu routes

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const authRoutes = require("./authRoutes");
 //const orderRoutes = require('./orderRoutes');
-//const menuRoutes = require('./menuRoutes');
+const menuRoutes = require('./menuRoutes');
 //const reportRoutes = require('./reportRoutes');
 
 router.use("/", authRoutes);
 //router.use('/orders', orderRoutes);
-//router.use('/menu', menuRoutes);
+router.use('/menu', menuRoutes);
 //router.use('/reports', reportRoutes);
 
 module.exports = router;
