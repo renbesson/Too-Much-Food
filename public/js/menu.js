c5d1b2f266ab7e5090d554415788c68a0ca866ca
mdpais <mdpais@gmail.com>
2023-02-19 18:30:39 -0400

    all functions on menu page working properly

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,3 +1,5 @@
+//CREATE MENU ITEM
+
 const createMenuItemFormHandler = async (event) => {
   event.preventDefault();
 
@@ -17,7 +19,6 @@ const createMenuItemFormHandler = async (event) => {
       location.reload();
     } else {
       const resJson = await response.json();
-      console.log(resJson);
       alert(`${response.statusText}\r${resJson.message}`);
     }
   }
@@ -25,38 +26,81 @@ const createMenuItemFormHandler = async (event) => {
 
 document.getElementById("createItemBtn").addEventListener("click", createMenuItemFormHandler);
 
-let isEditing = null;
+// ID TO MODIFY MENU ITEM
 
-const editItem = async (event) => {
-  isEditing = true;
-  console.log(event.target.id);
-};
+var id = null;
 
-////////////////////////////////////////////////////////////////////////
-// Deletes a specific item from the menu
-////////////////////////////////////////////////////////////////////////
-const deleteItem = async (event) => {
-  const id = event.target.id.split("-")[1];
+// ACTIVATE EDIT FORM
 
-  const response = await fetch(`/api/menu/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-  });
+const editItemForm = async (event) => {
+  id = event.target.id.split("-")[1];
+  const menu_item = document.getElementById("item-"+id).textContent;
+  const get_price = document.getElementById("price-"+id).textContent;
+  const menu_price = parseFloat(get_price.substring(8));
+  const menu_active = document.getElementById("active-"+id).textContent;
+  document.getElementById("editItemBtn").style.display = "block";
+  document.getElementById("editHead").style.display = "block";
+  document.getElementById("createItemBtn").style.display = "none";
+  document.getElementById("createHead").style.display = "none";
+  document.getElementById("cancelBtn").style.display = "block";
 
-  if (response.ok) {
-    alert("Item deleted successfully!");
-    location.reload();
-  } else {
-    const resJson = await response.json();
-    console.log(resJson);
-    alert(`${response.statusText}\r${resJson.message}`);
+  document.getElementById("item").value = menu_item;
+  document.getElementById("price").value = menu_price;
+  if (menu_active == "Active") {
+    document.getElementById("isActive").checked = true;
   }
+  else {
+    document.getElementById("isActive").checked = false;
+  }
+  
 };
 
 const editCardsBtn = document.querySelectorAll(".editBtn");
+editCardsBtn.forEach((card) => card.addEventListener("click", editItemForm));
 
-const deleteCardsBtn = document.querySelectorAll(".deleteBtn");
+// CANCEL EDIT AND SWITCH BACK TO CREATE FORM
 
-editCardsBtn.forEach((card) => card.addEventListener("click", editItem));
+const cancelBtnHandler = async () => {
+  event.preventDefault();
+  
+  document.getElementById("editItemBtn").style.display = "none";
+  document.getElementById("editHead").style.display = "none";
+  document.getElementById("createItemBtn").style.display = "block";
+  document.getElementById("createHead").style.display = "block";
+  document.getElementById("cancelBtn").style.display = "none";
 
-deleteCardsBtn.forEach((card) => card.addEventListener("click", deleteItem));
+  document.getElementById("item").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("isActive").checked = false;
+};
+
+document.getElementById("cancelBtn").addEventListener("click", cancelBtnHandler);
+
+
+// EDIT MENU ITEM
+
+const editItem = async (event) => {
+  event.preventDefault();
+
+  const item = document.getElementById("item").value.trim();
+  const price = document.getElementById("price").value.trim();
+  const active = document.getElementById("isActive").checked;
+
+  if (item && price) {
+    const response = await fetch(`/api/menu/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({ item, price, active }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      alert("Item modified successfully!");
+      location.reload();
+    } else {
+      const resJson = await response.json();
+      alert(`${response.statusText}\r${resJson.message}`);
+    }
+  }
+};
+
+document.getElementById("editItemBtn").addEventListener("click", editItem);
