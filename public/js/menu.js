d2a689bbc6ac3688d0187f6aef68c9f85f4d4925
Ren Besson <ren.besson@outlook.com>
2023-02-21 16:25:26 -0400

    Optimizing element creation

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,19 +3,19 @@
 const createMenuItemFormHandler = async (event) => {
   event.preventDefault();
 
-  const item = document.getElementById("item").value.trim();
-  const price = document.getElementById("price").value.trim();
-  const active = document.getElementById("isActive").checked;
+  const item = document.getElementById('item').value.trim();
+  const price = document.getElementById('price').value.trim();
+  const active = document.getElementById('isActive').checked;
 
   if (item && price) {
-    const response = await fetch("/api/menu", {
-      method: "POST",
+    const response = await fetch('/api/menu', {
+      method: 'POST',
       body: JSON.stringify({ item, price, active }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      alert("Item created successfully!");
+      alert('Item created successfully!');
       location.reload();
     } else {
       const resJson = await response.json();
@@ -24,7 +24,7 @@ const createMenuItemFormHandler = async (event) => {
   }
 };
 
-document.getElementById("createItemBtn").addEventListener("click", createMenuItemFormHandler);
+document.getElementById('createItemBtn').addEventListener('click', createMenuItemFormHandler);
 
 // ID TO MODIFY MENU ITEM
 
@@ -33,68 +33,65 @@ var id = null;
 // ACTIVATE EDIT FORM
 
 const editItemForm = async (event) => {
-  id = event.target.id.split("-")[1];
-  const menu_item = document.getElementById("item-"+id).textContent;
-  const get_price = document.getElementById("price-"+id).textContent;
+  id = event.target.id.split('-')[1];
+  const menu_item = document.getElementById('item-' + id).textContent;
+  const get_price = document.getElementById('price-' + id).textContent;
   const menu_price = parseFloat(get_price.substring(8));
-  const menu_active = document.getElementById("active-"+id).textContent;
-  document.getElementById("editItemBtn").style.display = "block";
-  document.getElementById("editHead").style.display = "block";
-  document.getElementById("createItemBtn").style.display = "none";
-  document.getElementById("createHead").style.display = "none";
-  document.getElementById("cancelBtn").style.display = "block";
+  const menu_active = document.getElementById('active-' + id).textContent;
+  document.getElementById('editItemBtn').style.display = 'block';
+  document.getElementById('editHead').style.display = 'block';
+  document.getElementById('createItemBtn').style.display = 'none';
+  document.getElementById('createHead').style.display = 'none';
+  document.getElementById('cancelBtn').style.display = 'block';
 
-  document.getElementById("item").value = menu_item;
-  document.getElementById("price").value = menu_price;
-  if (menu_active == "Active") {
-    document.getElementById("isActive").checked = true;
+  document.getElementById('item').value = menu_item;
+  document.getElementById('price').value = menu_price;
+  if (menu_active == 'Active') {
+    document.getElementById('isActive').checked = true;
+  } else {
+    document.getElementById('isActive').checked = false;
   }
-  else {
-    document.getElementById("isActive").checked = false;
-  }
-  
 };
 
-const editCardsBtn = document.querySelectorAll(".editBtn");
-editCardsBtn.forEach((card) => card.addEventListener("click", editItemForm));
+const editCardsBtn = document.querySelectorAll('.editBtn');
+editCardsBtn.forEach((card) => card.addEventListener('click', editItemForm));
 
 // CANCEL EDIT AND SWITCH BACK TO CREATE FORM
 
 const cancelBtnHandler = async () => {
   event.preventDefault();
-  
-  document.getElementById("editItemBtn").style.display = "none";
-  document.getElementById("editHead").style.display = "none";
-  document.getElementById("createItemBtn").style.display = "block";
-  document.getElementById("createHead").style.display = "block";
-  document.getElementById("cancelBtn").style.display = "none";
 
-  document.getElementById("item").value = "";
-  document.getElementById("price").value = "";
-  document.getElementById("isActive").checked = false;
+  document.getElementById('editItemBtn').style.display = 'none';
+  document.getElementById('editHead').style.display = 'none';
+  document.getElementById('createItemBtn').style.display = 'block';
+  document.getElementById('createHead').style.display = 'block';
+  document.getElementById('cancelBtn').style.display = 'none';
+
+  document.getElementById('item').value = '';
+  document.getElementById('price').value = '';
+  document.getElementById('isActive').checked = false;
 };
 
-document.getElementById("cancelBtn").addEventListener("click", cancelBtnHandler);
-
+document.getElementById('cancelBtn').addEventListener('click', cancelBtnHandler);
 
 // EDIT MENU ITEM
 
 const editItem = async (event) => {
   event.preventDefault();
 
-  const item = document.getElementById("item").value.trim();
-  const price = document.getElementById("price").value.trim();
-  const active = document.getElementById("isActive").checked;
+  const item = document.getElementById('item').value.trim();
+  const price = document.getElementById('price').value.trim();
+  const active = document.getElementById('isActive').checked;
 
   if (item && price) {
     const response = await fetch(`/api/menu/${id}`, {
-      method: "PUT",
+      method: 'PUT',
       body: JSON.stringify({ item, price, active }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      alert("Item modified successfully!");
+      alert('Item modified successfully!');
       location.reload();
     } else {
       const resJson = await response.json();
@@ -103,4 +100,4 @@ const editItem = async (event) => {
   }
 };
 
-document.getElementById("editItemBtn").addEventListener("click", editItem);
+document.getElementById('editItemBtn').addEventListener('click', editItem);
--- public/js/orders.js
+++ public/js/orders.js
@@ -9,22 +9,22 @@ let platesSelected = [];
 const createOrderFormHandler = async (event) => {
   event.preventDefault();
 
-  const table_no = document.getElementById("table_no").value.trim();
-  const active = document.getElementById("isComplete").checked;
+  const table_no = document.getElementById('table_no').value.trim();
+  const active = document.getElementById('isComplete').checked;
 
   if (platesSelected.length <= 0) {
-    alert("You need to select at least one plate.");
+    alert('You need to select at least one plate.');
     return;
   }
   if (table_no) {
-    const response = await fetch("/api/orders", {
-      method: "POST",
+    const response = await fetch('/api/orders', {
+      method: 'POST',
       body: JSON.stringify({ table_no, completed: active, menuIds: platesSelected }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      alert("Order created successfully!");
+      alert('Order created successfully!');
       location.reload();
     } else {
       alert(`Error ${response.status}\n${response.statusText}`);
@@ -32,14 +32,14 @@ const createOrderFormHandler = async (event) => {
   }
 };
 
-document.getElementById("createOrderBtn").addEventListener("click", createOrderFormHandler);
+document.getElementById('createOrderBtn').addEventListener('click', createOrderFormHandler);
 
 /////////////////////////////////////////////////////////////////////
 // Gets all plates
 /////////////////////////////////////////////////////////////////////
 const getPlates = async () => {
-  const response = await fetch("/api/menu/menuJson", {
-    method: "GET",
+  const response = await fetch('/api/menu/menuJson', {
+    method: 'GET',
   });
 
   if (response.ok) {
@@ -54,11 +54,11 @@ const getPlates = async () => {
 // Loads all plate options to the select input
 /////////////////////////////////////////////////////////////////////
 let loadPlates = async () => {
-  const plateSelect = document.getElementById("plateSelect");
+  const plateSelect = document.getElementById('plateSelect');
   const plates = await getPlates();
 
   plates.forEach((plate) => {
-    const option = document.createElement("option");
+    const option = document.createElement('option');
     option.id = plate.id;
     option.text = plate.item;
 
@@ -75,43 +75,43 @@ loadPlates();
 const addPlate = (event) => {
   event.preventDefault();
 
-  const selectedPlatesTable = document.getElementById("selectedPlatesTable");
-  const plateSelect = document.getElementById("plateSelect");
-  const qty = document.getElementById("qty");
+  const selectedPlatesTable = document.getElementById('selectedPlatesTable');
+  const plateSelect = document.getElementById('plateSelect');
+  const qty = document.getElementById('qty');
 
-  const newPlate = document.createElement("tr");
-  const newPlateName = document.createElement("td");
-  const newPlateQty = document.createElement("td");
-  const newPlateDeleteBtn = document.createElement("button");
+  const newPlate = document.createElement('tr');
+  const newPlateName = document.createElement('td');
+  const newPlateQty = document.createElement('td');
+  const newPlateDeleteBtn = document.createElement('button');
 
-  newPlate.classList.add("grid", "grid-cols-6");
-  newPlateName.classList.add("col-span-4");
-  newPlateQty.classList.add("col-span-1");
-  newPlateDeleteBtn.classList.add("btn", "btn-error", "col-span-1", "deletePlateBtn");
+  newPlate.classList.add('grid', 'grid-cols-6');
+  newPlateName.classList.add('col-span-4');
+  newPlateQty.classList.add('col-span-1');
+  newPlateDeleteBtn.classList.add('btn', 'btn-error', 'col-span-1', 'deletePlateBtn');
 
   newPlateName.textContent = plateSelect.value;
   newPlateQty.textContent = qty.value;
-  newPlateDeleteBtn.textContent = "X";
+  newPlateDeleteBtn.textContent = 'X';
 
   newPlate.id = `plate-${plateSelect.selectedIndex}`;
   newPlateDeleteBtn.id = `delete-${plateSelect.selectedIndex}`;
 
-  newPlateDeleteBtn.addEventListener("click", deletePlate);
+  newPlateDeleteBtn.addEventListener('click', deletePlate);
 
   newPlate.append(newPlateName, newPlateQty, newPlateDeleteBtn);
 
   selectedPlatesTable.append(newPlate);
 
   const plateOption = document.getElementById(plateSelect.selectedIndex);
-  plateOption.style.display = "none";
+  plateOption.style.display = 'none';
 
   //cleanup
   platesSelected.push({ item_id: plateSelect.selectedIndex, qty: qty.value });
-  plateSelect.value = "Pick a plate";
+  plateSelect.value = 'Pick a plate';
   qty.value = 1;
 };
 
-document.getElementById("addPlateBtn").addEventListener("click", addPlate);
+document.getElementById('addPlateBtn').addEventListener('click', addPlate);
 
 /////////////////////////////////////////////////////////////////////
 // Event listener for Delete plate button
@@ -119,35 +119,69 @@ document.getElementById("addPlateBtn").addEventListener("click", addPlate);
 const deletePlate = (event) => {
   event.preventDefault();
 
-  const id = event.target.id.split("-")[1];
+  const id = event.target.id.split('-')[1];
+
   const plateSelect = document.getElementById(`plate-${id}`);
   const plateOption = document.getElementById(id);
 
   plateSelect.remove();
-  plateOption.style.display = "block";
+  plateOption.style.display = 'block';
 
   platesSelected = platesSelected.filter((item) => item.item_id != id);
 };
 
-// ACTIVATE EDIT FORM
+/////////////////////////////////////////////////////////////////////
+// Event listener for Edit order button
+/////////////////////////////////////////////////////////////////////
+const editOrder = (event) => {
+  event.preventDefault();
 
-const editItemForm = async (event) => {
-  id = event.target.id.split("-")[1];
-  const menu_item = document.getElementById("item-" + id).textContent;
-  const get_price = document.getElementById("price-" + id).textContent;
-  const menu_price = parseFloat(get_price.substring(8));
-  const menu_active = document.getElementById("active-" + id).textContent;
-  document.getElementById("editItemBtn").style.display = "block";
-  document.getElementById("editHead").style.display = "block";
-  document.getElementById("createItemBtn").style.display = "none";
-  document.getElementById("createHead").style.display = "none";
-  document.getElementById("cancelBtn").style.display = "block";
+  const id = event.target.id.split('-')[1];
+  const order = JSON.parse(event.target.dataset.order);
+  console.log(order);
 
-  document.getElementById("item").value = menu_item;
-  document.getElementById("price").value = menu_price;
-  if (menu_active == "Active") {
-    document.getElementById("isActive").checked = true;
-  } else {
-    document.getElementById("isActive").checked = false;
-  }
+  const table_no = document.getElementById('table_no');
+  const active = document.getElementById('isComplete');
+  const plateSelect = document.getElementById(`plate-${id}`);
+  const plateOption = document.getElementById(id);
+
+  table_no.value = order.table_no;
+  active.checked = order.completed;
+
+  const tableBody = document.getElementById('selectedPlatesTable');
+  tableBody.innerHTML = '';
+
+  order.orderedItems.forEach((item) => {
+    const orderedItem = document.createElement('tr');
+    orderedItem.id = `plate-${item.id}`;
+    const deleteBtn = `<button class="btn btn-error col-span-1 deletePlateBtn" id="delete-${item.id}">X</button>`;
+
+    orderedItem.classList.add('grid', 'grid-cols-6');
+    orderedItem.innerHTML = `
+    <th class="col-span-4">${item.menu.item}</th> 
+    <th class="col-span-1">${item.quantity}</th> 
+    <th class="col-span-1">${deleteBtn}</th> 
+ `;
+
+    tableBody.appendChild(orderedItem);
+  });
+  //plateSelect.remove();
+  //plateOption.style.display = 'block';
+
+  //platesSelected = platesSelected.filter((item) => item.item_id != id);
+  refreshELs();
 };
+
+/////////////////////////////////////////////////////////////////////
+// Event listener refresher
+/////////////////////////////////////////////////////////////////////
+const refreshELs = () => {
+  console.log('dasd');
+  const editBtns = document.querySelectorAll('.editBtn');
+  editBtns.forEach((btn) => btn.addEventListener('click', editOrder));
+
+  const deleteBtns = document.querySelectorAll('.deletePlateBtn');
+  deleteBtns.forEach((btn) => btn.addEventListener('click', deletePlate));
+};
+
+refreshELs();
