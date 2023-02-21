b0a1e690b26ac0e029cf08ba6a9f32398d5e4a1e
Ren Besson <ren.besson@outlook.com>
2023-02-20 22:57:54 -0400

    Testing adding options to plate select

--- public/js/orders.js
+++ public/js/orders.js
@@ -1,3 +1,5 @@
+const { Menu, User } = require("../../models");
+
 //CREATE ORDER
 
 const createOrderFormHandler = async (event) => {
@@ -24,3 +26,46 @@ const createOrderFormHandler = async (event) => {
 };
 
 document.getElementById("createOrderBtn").addEventListener("click", createOrderFormHandler);
+
+const getPlates = async () => {
+  try {
+    const menuData = await Menu.findAll();
+    const menuItems = menuData.map((menu) => menu.get({ plain: true }));
+
+    return menuItems;
+  } catch (error) {
+    return { error: error };
+  }
+};
+
+let loadPlates = () => {
+  const plateSelect = document.getElementById("plateSelect");
+  getPlates().forEach((plate) => {
+    console.log(plate);
+  });
+};
+
+loadPlates();
+
+// ACTIVATE EDIT FORM
+
+const editItemForm = async (event) => {
+  id = event.target.id.split("-")[1];
+  const menu_item = document.getElementById("item-" + id).textContent;
+  const get_price = document.getElementById("price-" + id).textContent;
+  const menu_price = parseFloat(get_price.substring(8));
+  const menu_active = document.getElementById("active-" + id).textContent;
+  document.getElementById("editItemBtn").style.display = "block";
+  document.getElementById("editHead").style.display = "block";
+  document.getElementById("createItemBtn").style.display = "none";
+  document.getElementById("createHead").style.display = "none";
+  document.getElementById("cancelBtn").style.display = "block";
+
+  document.getElementById("item").value = menu_item;
+  document.getElementById("price").value = menu_price;
+  if (menu_active == "Active") {
+    document.getElementById("isActive").checked = true;
+  } else {
+    document.getElementById("isActive").checked = false;
+  }
+};
